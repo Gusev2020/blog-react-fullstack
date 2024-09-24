396d0280c793341434f8121bf6bf7662a067091f
Gusev2020 <gusev12.gusev@yandex.ru>
2024-09-24 22:42:52 +0300

    postController. Добавил метод getAllPost

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,45 +1,62 @@
 const { prisma } = require('../prisma/prisma-client')
 
-
 const PostController = {
-  createPost: async (req, res)=>{
+  createPost: async (req, res) => {
     const { content } = req.body
 
     const authorId = req.user.userId
 
-    if(!content) {
-      return res
-      .status(400)
-      .json({ error: 'Все поля обязательны' })
+    if (!content) {
+      return res.status(400).json({ error: 'Все поля обязательны' })
     }
 
     try {
-
       const post = await prisma.post.create({
         data: {
           content,
-          authorId
-        }
+          authorId,
+        },
       })
 
       res.json(post)
-
-    } catch(e) {
+    } catch (e) {
       console.error('Error in create post', e)
       res.status(500).json({ error: 'internal server error' })
     }
+  },
+  getAllPosts: async (req, res) => {
 
+    const userId = req.user.userId
+
+    try {
+      const posts = await prisma.post.findMany({
+        include: {
+          likes: true,
+          auhtor: true,
+          comments: true
+        },
+        orderBy: {
+          createdAt: 'desc'
+        }
+      })
+
+      const postWithLikeInfo = posts.map(post => ({
+        ...post,
+        likeByUser: post.likes.some(like => like.userId === userId)
+      }))
+
+      res.json(postWithLikeInfo)
+    } catch(e) {
+      console.error('Error in get all post', e)
+      res.status(500).json({ error: 'internal server error' })
+    }
   },
-  getAllPosts: async (req, res)=>{
-    res.send('get all posts')
-  },
-  getpostById: async (req, res)=>{
+  getpostById: async (req, res) => {
     res.send('get post by id')
   },
-  deletePost: async (req, res)=>{
+  deletePost: async (req, res) => {
     res.send('delete post')
   },
-
 }
 
 module.exports = PostController
