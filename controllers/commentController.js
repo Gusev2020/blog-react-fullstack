26e6adad941a2f7a0d90eb2cdad6e6e66199a62a
Gusev2020 <gusev12.gusev@yandex.ru>
2024-09-24 23:34:15 +0300

    commentController. Добавил метод deleteComment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -5,28 +5,47 @@ const CommnetController = {
     const { postId, content } = req.body
     const userId = req.user.userId
 
-    if(!postId || !content) {
+    if (!postId || !content) {
       return res.status(400).json({ error: 'Все поля обязательны' })
     }
 
     try {
       const comment = await prisma.comment.create({
-        data:{
+        data: {
           postId,
           userId,
-          content
-        }
+          content,
+        },
       })
-
       res.json(comment)
-    } catch(e) {
+    } catch (e) {
       console.error('Error in create comment', e)
       res.status(500).json({ error: 'internal server error' })
     }
-
   },
   deleteComment: async (req, res) => {
-    res.send('deleteComment')
+    const { id } = req.params
+    const userId = req.user.userId
+
+    try {
+      const comment = await prisma.comment.findUnique({
+        where: { id },
+      })
+  
+      if (!comment) {
+        return res.status(404).json({ error: 'Комментарий не найден' })
+      }
+  
+      if (comment.userId !== userId) {
+        return res.status(403).json({ error: 'Нет доступа' })
+      }
+      
+      const deletePost = await prisma.comment.delete({ where: { id } })
+      res.json(deletePost)
+    } catch (e) {
+      console.error('Error in delete comment by id', e)
+      res.status(500).json({ error: 'internal server error' })
+    }
   },
 }
 
